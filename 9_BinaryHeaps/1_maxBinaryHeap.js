eac7d960afb76630a4f869c774837dedda9faa9a
Nishanth Namana <nishanth.namana@gmail.com>
2023-09-06 08:27:07 -0700

    Added Binary Heap

--- 9_BinaryHeaps/1_maxBinaryHeap.js
+++ 9_BinaryHeaps/1_maxBinaryHeap.js
@@ -1,4 +1,4 @@
-class BinaryHeap {
+class MaxBinaryHeap {
     constructor() {
         this.values = [];
     }
@@ -7,13 +7,9 @@ class BinaryHeap {
         let heap = this.values;
         heap.push(val);
 
-        if (heap.length === 1) {
-            return this.value;
-        }
-
         let index = heap.length - 1;
 
-        while (index >= 0) {
+        while (index > 0) {
             let parentIndex = Math.floor((index - 1) / 2);
 
             if (heap[index] > heap[parentIndex]) {
@@ -24,13 +20,56 @@ class BinaryHeap {
 
                 index = parentIndex;
             } else {
-                return this.value;
+                return this.values;
             }
         }
+
+        return this.values;
+    }
+
+    extractMax() {
+        let heap = this.values;
+
+        let max = heap[0];
+
+        if (heap.length > 1) {
+            heap[0] = heap.pop();
+        } else {
+            heap.pop();
+            return max;
+        }
+
+        let index = 0;
+
+        //Bubble-Down
+        while (index < heap.length) {
+            let lc = 2 * index + 1;
+            let rc = 2 * index + 2;
+            let swapIdx = null;
+
+            if (lc < heap.length && rc < heap.length) {
+                if (heap[lc] > heap[index] && heap[rc] > heap[index]) {
+                    swapIdx = heap[lc] > heap[rc] ? lc : rc;
+                } else if (heap[lc] > heap[index]) {
+                    swapIdx = lc;
+                } else if (heap[rc] > heap[index]) {
+                    swapIdx = rc;
+                } else {
+                    break;
+                }
+
+                [heap[index], heap[swapIdx]] = [heap[swapIdx], heap[index]];
+                index = swapIdx;
+            } else {
+                break;
+            }
+        }
+
+        return max;
     }
 }
 
-let heap = new BinaryHeap();
+let heap = new MaxBinaryHeap();
 
 heap.insert(41);
 heap.insert(39);
@@ -43,4 +82,12 @@ console.log(heap.values); // [ 41, 39, 33, 18, 27, 12 ]
 
 heap.insert(55);
 
-console.log(heap.values); // [ 55, 39, 41, 18, 27, 12, 33 ]
+heap.insert(1);
+
+console.log(heap.values); // [ 55, 39, 41, 18, 27, 12, 33, 1 ]
+
+heap.insert(100);
+
+console.log(heap.values); // [100, 55, 41, 39, 27, 12, 33, 1, 18]
+
+heap.extractMax(); // [55, 39, 41, 18, 27, 12, 33, 1]
